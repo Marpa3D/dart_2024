1a8ca4e833f5f30b4db75a046ce4262db26154e0
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2024-07-15 07:34:20 +0300

    Чтение файла csv построчно и вывод в консоль

--- data_processing/totals.dart
+++ data_processing/totals.dart
@@ -1,3 +1,14 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print(arguments);
+  if (arguments.isEmpty) {
+    print('Введите в формате: dart totals.dart <inputFile.csv>');
+    exit(1);
+  }
+  // Чтение файла построчно и вывод в консоль
+  final inputFile = arguments.first;
+  final lines = File(inputFile).readAsLinesSync();
+  for (var line in lines) {
+    print(line);
+  }
 }
